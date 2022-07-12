ea26d70fa95b501538f886c693bba2a7fddffc9c
Melis Belekov <apacho.melis@gmail.com>
2022-07-11 22:48:02 -0500

    Commited lol

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def blog(request):
+    articles = Article.objects.order_by('-blogDate')
+    return render(request, 'blog/all_blogs.html', {'articles': articles})
