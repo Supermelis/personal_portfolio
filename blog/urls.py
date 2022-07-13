265b3df72da8a4fbf365bb6e9c6fc62f5a487fdd
Melis Belekov <apacho.melis@gmail.com>
2022-07-13 12:51:15 -0500

    almost done with basic structure

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path('', views.blog),
+    path('', views.blog, name="all_blogs"),
+    path('<int:article_id>/', views.detail, name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
 def blog(request):
     articles = Article.objects.order_by('-blogDate')
     return render(request, 'blog/all_blogs.html', {'articles': articles})
+
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'blog/detail.html', {'article': article})
